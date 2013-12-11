851a8bdb2d3ad147521ea5ee01afa6546c468b41
Matt Webb <matt@interconnected.org>
2013-12-11 12:54:29 +0000

    added DB boilerplate

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # initialization
 app = Flask(__name__)
 app.config.update(
 	DEBUG = True,
+	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'],
 )
+
+db = SQLAlchemy(app)
 
 # controllers
 @app.route("/")
