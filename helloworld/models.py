76af59cd15f9480aa5ae2f01a7e697e5d45da355
Matt Webb <matt@interconnected.org>
2013-12-12 18:30:59 +0000

    making models to implement twitter sign-in

--- helloworld/models.py
+++ helloworld/models.py
@@ -1,26 +1,51 @@
-#>>> from yourapplication import db
-#>>> db.create_all()
+from flask.ext.security import UserMixin, RoleMixin
 
 from . import db
 
-ROLE_USER = 0
-ROLE_ADMIN = 1
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
+
+class Role(db.Model, RoleMixin):
+
+	__tablename__ = "roles"
+
+	id = db.Column(db.Integer(), primary_key=True)
+	name = db.Column(db.String(80), unique=True)
+	description = db.Column(db.String(255))
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	nickname = db.Column(db.String(64), unique = True)
-	email = db.Column(db.String(120), unique = True)
-	role = db.Column(db.SmallInteger, default = ROLE_USER)
-	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
+	__tablename__ = "users"
+
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(255), unique=True)
+	password = db.Column(db.String(120))
+	active = db.Column(db.Boolean())
+	last_login_at = db.Column(db.DateTime())
+	current_login_at = db.Column(db.DateTime())
+	last_login_ip = db.Column(db.String(100))
+	current_login_ip = db.Column(db.String(100))
+	login_count = db.Column(db.Integer)
+	roles = db.relationship('Role', secondary=roles_users,
+		backref=db.backref('users', lazy='dynamic'))
+	connections = db.relationship('Connection',
+		backref=db.backref('user', lazy='joined'), cascade="all")
 
 	def __repr__(self):
-		return '<User %r>' % (self.nickname)
+		return '<User %r>' % (self.email)
 
-class Post(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	body = db.Column(db.String(140))
-	timestamp = db.Column(db.DateTime)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Connection(db.Model):
 
-	def __repr__(self):
-		return '<Post %r>' % (self.body)
+	__tablename__ = "connections"
+	
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+	provider_id = db.Column(db.String(255))
+	provider_user_id = db.Column(db.String(255))
+	access_token = db.Column(db.String(255))
+	secret = db.Column(db.String(255))
+	display_name = db.Column(db.String(255))
+	profile_url = db.Column(db.String(512))
+	image_url = db.Column(db.String(512))
+	rank = db.Column(db.Integer)
--- helloworld/views.py
+++ helloworld/views.py
@@ -1,7 +1,18 @@
-from . import app
 from flask import render_template
+
+from . import app
+#from .forms import RegisterForm
 
 @app.route("/")
 @app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated():
+		return redirect(request.referrer or "/")
+	
+	#form = RegisterForm()
+	
+	return render_template('register.html')
